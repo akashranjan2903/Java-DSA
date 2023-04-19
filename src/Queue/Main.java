9ff3aa5acb0d312145750c07e6f207e0e032b4f1
akashranjan2903 <akashkr2903@gmail.com>
2023-04-20 02:22:05 +0530

    decimal to binary from queue

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -1,5 +1,9 @@
 package src.Queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 
 public class Main {
@@ -25,5 +29,23 @@ public class Main {
     cQueue.display();
     cQueue.reverse();
     cQueue.display();
+
+    // print binary of n numbers using queue
+
+    Queue<String> binary = new LinkedList<String>();
+    ArrayList list = new ArrayList<>();
+    binary.add("1");
+    for(int i=0;i<10;i++) {
+      String str = binary.remove();
+      list.add(str); // add 1 to list
+
+      // now adding 0 and 1 in 1 will give 2 and 3 and so on
+      binary.add(str+"0"); // add 0 to binary queue
+      binary.add(str+"1"); // add 1 to binary queue
+    }
+    //print the list
+    for(int i=0;i<10;i++) {
+       System.out.println(list.get(i));
+    }
 }
 }
